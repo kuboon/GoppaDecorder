17ee052f665f397d53c768c031f8f0e633bd921a
test <test@chagai>
2020-03-03 18:34:06 +0900

    hashvalue

--- decomp/oplib.c
+++ decomp/oplib.c
@@ -21,6 +21,7 @@
 #include "global.h"
 #include "struct.h"
 
+#include "sha3.c"
 
 extern unsigned long xor128(void);
 extern int mlt(int x,int y);
@@ -1678,9 +1679,44 @@ void keygen (unsigned short *g){
 }
 
 
-void
-encrypt ()
+static void byte_to_hex(uint8_t b, char s[23]) {
+    unsigned i=1;
+    s[0] = s[1] = '0';
+    s[2] = '\0';
+    while(b) {
+        unsigned t = b & 0x0f;
+        if( t < 10 ) {
+            s[i] = '0' + t;
+        } else {
+            s[i] = 'a' + t - 10;
+        }
+        i--;
+        b >>= 4;
+    }
+}
+
+
+void encrypt (unsigned char buf[])
 {
+  const uint8_t *hash;
+  sha3_context c;
+  int image_size=512,i;
+  //  char buf[8192];
+
+  puts(buf);
+  printf("\n");
+  //exit(1);
+  
+  //scanf("%s",buf);
+  sha3_Init256(&c);
+  sha3_Update(&c, (char *)buf, strlen(buf));
+  hash = sha3_Finalize(&c);
+  for(i=0; i<image_size/8; i++) {
+    char s[3];
+    byte_to_hex(hash[i],s);
+    printf("%s", s);
+  }
+    printf("\n");
 
 
 }
@@ -2266,6 +2302,16 @@ label:
 	    }
 	}
 
+      char buf[8192],buf1[10];
+      for(i=0;i<D;i++){
+	//buf1=(char*)zz[i];
+	snprintf(buf1, 10, "%d",zz[i] );
+	puts(buf1);
+	//printf("%c,",buf1);
+	strcat(buf,buf1);
+      }
+      encrypt(buf);
+      exit(1);
 
 
       printf ("zz=");
