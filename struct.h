71c4f5bd02ac0967d6e0231626410fc0080ae5ed
test <test@chagai>
2020-03-19 18:51:10 +0900

    modified

--- struct.h
+++ struct.h
@@ -1,14 +1,3 @@
-//符号のパラーメータの指定。通常[N,K,T]として、
-//Nは符号の長さ、Kが符号の次元、Tは訂正エラー数
-//を表す。ここではDは符号長にしている。
-#define N 6688 // (^^;)?
-#define M 8192 //有限体の元の数
-#define K (128*2) //符号の次元
-#define DEG (2*K) //多項式の次数
-#define T (K/2) //エラーの数
-#define E (13) //拡大体のビット数
-#define D (6688) //符号長（短縮符号）
-#define F 256*13 //2040
 
 //monomial
 typedef struct{
@@ -21,9 +10,15 @@ typedef struct{
   oterm t[DEG]; //単項式の配列として多項式を表現する
 } OP;
 
-typedef struct{
-  unsigned int x[DEG]; //配列の添字を次数に、配列の値を係数に持つ多項式の表現
+typedef union {
+  unsigned short x[DEG]; //配列の添字を次数に、配列の値を係数に持つ多項式の表現
+  unsigned long long int e[DEG/4];
 } vec;
+
+typedef union {
+  unsigned short v[N];
+  unsigned long long int u[N/4];
+} MT;
 
 //extra gcd
 typedef struct{
